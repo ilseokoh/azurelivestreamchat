4b964a638ca66fbc93271eb94bfdd899c9c418f6
오일석 <Kevin@KevinMac.local>
2016-11-16 16:54:04 +0900

    modify chat page

--- routes/chat.js
+++ routes/chat.js
@@ -3,8 +3,18 @@ var router = express.Router();
 
 /* GET chat page. */
 
+
+
 router.get('/', function(req, res) {
-  res.render('chat', { title: 'Live Stream Chat - Chat', username: req.session.username });
+  var model = {
+    title: "Asiance Hackthon Live!!",
+    description: "Asiance and Microsoft get toghther to hack for good. Wed 16 Nov at Microsoft office.", 
+    deleted: false, 
+    streamurl: "http://livestreamchat.streaming.mediaservices.windows.net/bc9a81e1-14d2-4928-81cf-2eaeb865993e/93a7bea1-e675-4ccc-86d8-898f5b6e6673.ism/manifest",
+    username: req.session.username
+  };
+
+  res.render('chat', model);
 });
 
 module.exports = router;
